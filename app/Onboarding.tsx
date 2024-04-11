bda4e376f42a4543be0d765d3c70af86c2317dfc
Franklin-pro <136065526+Franklin-pro@users.noreply.github.com>
2024-04-11 09:42:29 +0200

    onboarding

--- app/Onboarding.tsx
+++ app/Onboarding.tsx
@@ -3,7 +3,7 @@ import OnBording from '../components/Onboarding/Onbording'
 import MyComponent from "../components/cards";
 import Cards from "../components/cards";
 import CardProps from "../components/UI/cardsprops";
-import HomeScreen from "./HomeScreen";
+
 
 
 function Onboarding(){
