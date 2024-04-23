6bebb82564535f1c1f6303d9eb6154418b062649
mr3nz1 <88678347+mr3nz1@users.noreply.github.com>
2024-04-23 19:26:21 +0200

    Appwrite Configuration (#14)
    * ft: setup basic functionalities of the booking wheel
    
    * ft:choose duration wheel on the booking screen
    
    * ft:Appwrite setup
    
    * ft:Appwrite setup

--- appwrite/config.tsx
+++ appwrite/config.tsx
@@ -3,6 +3,6 @@ import { Account, Client } from "react-native-appwrite/src";
 let client = new Client();
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("6624e79e13678a3f6e7d");
+  .setProject("6627e810cfd8765e7b01");
 
 export const account: Account = new Account(client);
