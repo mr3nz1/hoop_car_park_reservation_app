c84fbf7260719d967c6b0b5b3f14a46ecde3ae08
Murenzi Paterne <murenzi419@gmail.com>
2024-04-29 14:42:50 +0200

    ch: added removed components to this branch

--- app/(app)/Parking/DetailCategory.tsx
+++ app/(app)/Parking/DetailCategory.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import CardsProps from "../../../components/UI/ParkingCard";
 import cardData from "../../../cards";
 import { ScrollView, Image, Pressable } from "react-native";
@@ -8,9 +8,47 @@ import { View, StyleSheet } from "react-native";
 import Input from "../../../components/UI/Input";
 import { LocateIcon, Search, TagBg } from "../../../components/UI/Icons/Icons";
 import { StatusBar } from "expo-status-bar";
+import { databases } from "../../../appwrite/config";
+import { Query } from "react-native-appwrite/src";
+import { ActivityIndicator } from "react-native-paper";
+import ParkingCard from "../../../components/UI/ParkingCard";
+import { router } from "expo-router";
 
 export default function DetailCategory() {
   const [cards, setCards] = useState(cardData);
+  const [parkings, setParkings] = useState<
+  | {
+      id: string;
+      price: string;
+      timeAway: string;
+      name: string;
+      avenue: string;
+      imageUrl: string;
+    }[]
+  | any[]
+>([]);
+
+const loadParkings = useMemo(async () => {
+  const { documents } = await databases.listDocuments(
+    "6627e9abef0db39e0ebf",
+    "6627e9cd3cc6db2ea8e3",
+    [Query.limit(6)]
+  );
+  documents.forEach((current) => {
+    setParkings((prevValue) => {
+      return [
+        ...prevValue,
+        {
+          id: current.$id,
+          price: current.price,
+          avenue: current.avenue,
+          imageUrl: current.image_url,
+          name: current.name,
+        },
+      ];
+    });
+  });
+}, []);
   return (
     <>
       <StatusBar style="dark" />
@@ -21,6 +59,10 @@ export default function DetailCategory() {
             btnLeft={<Search color="#929298" />}
             backgroundColor="#EAEAF3"
             textInputStyle={{ color: "#929298" }}
+
+            onChangeText={(text) => {
+              // console.log("Text changed:", text);
+            }}
           />
         </View>
 
@@ -42,28 +84,64 @@ export default function DetailCategory() {
         </View>
 
         <View style={{ gap: 15 }}>
-          {cards.map((card) => (
-            <CardsProps
-              key={card.id}
-              children={<Image source={card.image} />}
-              head={card.title}
-              hello={card.greating}
-              hr={card.hr}
-              dollar={card.money}
-              time={card.time}
-            />
-          ))}
-          {cards.map((card) => (
-            <CardsProps
-              key={card.id}
-              children={<Image source={card.image} />}
-              head={card.title}
-              hello={card.greating}
-              hr={card.hr}
-              dollar={card.money}
-              time={card.time}
-            />
-          ))}
+        {parkings.length === 0 ? (
+          <ActivityIndicator color="black" size="large" />
+        ) : (
+          parkings.map((parking) => {
+            console.log(parking);
+            return (
+              <ParkingCard
+                key={parking.id}
+                children={
+                  <Image
+                    source={{
+                      uri: parking.imageUrl,
+                    }}
+                    height={120}
+                    width={100}
+                    style={{ borderRadius: 20 }}
+                  />
+                }
+                name={parking.name}
+                avenue={parking.avenue}
+                timeAway={"10 min"}
+                price={parking.price}
+                onPress={() => {
+                  router.push("/Parking/ParkingDetails");
+                }}
+              />
+            );
+          })
+        )}
+           {parkings.length === 0 ? (
+          <ActivityIndicator color="black" size="large" />
+        ) : (
+          parkings.map((parking) => {
+            console.log(parking);
+            return (
+              <ParkingCard
+                key={parking.id}
+                children={
+                  <Image
+                    source={{
+                      uri: parking.imageUrl,
+                    }}
+                    height={120}
+                    width={100}
+                    style={{ borderRadius: 20 }}
+                  />
+                }
+                name={parking.name}
+                avenue={parking.avenue}
+                timeAway={"10 min"}
+                price={parking.price}
+                onPress={() => {
+                  router.push("/Parking/ParkingDetails");
+                }}
+              />
+            );
+          })
+        )}
         </View>
       </ScrollView>
 
